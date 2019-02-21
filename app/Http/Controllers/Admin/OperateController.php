3b165cb57af5d66c8cebe0aa9e84b0c964b0cff0
jiangs <549467713@qq.com>
2019-02-21 16:42:05 +0800

    5.0.19

--- app/Http/Controllers/Admin/OperateController.php
+++ app/Http/Controllers/Admin/OperateController.php
@@ -18,12 +18,7 @@ use Illuminate\Http\Request;
 
 class OperateController extends Controller
 {
-
-    /**
-     * 用户列表
-     */
-    public function user (Request $request) {
-        
+    public function user(Request $request){
         if ($request->ajax()) {
             $sortName = $request->post("sortName");    //排序列名
             $sortOrder = $request->post("sortOrder");   //排序（desc，asc）
@@ -31,25 +26,38 @@ class OperateController extends Controller
             $pageSize = $request->post("pageSize");   //一页显示的条数
             $search = $request->post("search",'');  //搜索条件
 
-            $sql = "select m.id,m.realname,cu.company_id,cu.user_id,c.company_name,m.mobile,cu.join_time,cu.login_time,cu.is_admin";
-            // $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_d";
-            // $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0) as total_d";
-            // $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_c";
-            // $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1) as total_c";
-            // $sql .= ",(((select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))))/2) as total_j_d";
-            // $sql .= ",(((select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))))/2) as total_j_c";
-            $sql .= " from company as c inner join company_user as cu on cu.company_id=c.id";
-            $sql .= " inner join members as m on cu.user_id=m.id";
+//            $sql = "select m.id,m.realname,cu.company_id,cu.user_id,c.company_name,m.mobile,cu.join_time,cu.login_time,cu.is_admin";
+//            $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_d";
+//            $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0) as total_d";
+//            $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_c";
+//            $sql .= ",(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1) as total_c";
+//            $sql .= ",(((select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 0 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))))/2) as total_j_d";
+//            $sql .= ",(((select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(id) from admin_log as al where c.id=al.company_id and cu.user_id = al.user_id and type = 1 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))))/2) as total_j_c";
+//            $sql .= " from company as c inner join company_user as cu on cu.company_id=c.id";
+//            $sql .= " inner join members as m on cu.user_id=m.id";
 
-            if ($search) {
+
+            $sql = "select m.id,m.realname,cu.company_id,cu.user_id,c.company_name,m.mobile,cu.join_time,cu.login_time,cu.is_admin,";
+            $sql .= "sum((case when al.type = 0 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end)) as cur_month_d,";
+            $sql .= "sum((case when al.type = 0  then 1 else 0 end)) as total_d,";
+            $sql .= "sum((case when al.type = 1 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end)) as cur_month_c,";
+            $sql .= "sum((case when al.type = 1  then 1 else 0 end)) as total_c,";
+            $sql .= "(sum((case when al.type = 0 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end))+ sum((case when al.type = 0  and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(al.created_at,'%Y%m'))=1 then 1 else 0 end)))/2  as total_j_d,";
+            $sql .= "(sum((case when al.type = 1 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end))+ sum((case when al.type = 1  and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(al.created_at,'%Y%m'))=1 then 1 else 0 end)))/2  as total_j_c";
+            $sql .= " from company as c inner join company_user as cu on cu.company_id=c.id inner join members as m on cu.user_id=m.id left join admin_log as al on (cu.company_id = al.company_id and cu.user_id = al.user_id)";
+            $sql .= " where 1=1";
+
+            if($search){
                 $sql .= " and (c.company_name like '%". $search ."%'";
                 $sql .= " or m.mobile like '%". $search ."%' or m.realname like'%". $search ."%')";
             }
 
+            $sql .= " group by m.id,m.realname,cu.company_id,cu.user_id,c.company_name,m.mobile,cu.join_time,cu.login_time,cu.is_admin";
+
             $data['total'] = count(DB::select($sql));
 
             $sql .= " order by ". $sortName ." ". $sortOrder;
-            if ($pageSize != 'All') {
+            if($pageSize != 'All'){
                 $start = ($pageNumber-1)*$pageSize;   //开始位置
                 $sql .= " limit ". $start .','. $pageSize;
             }
@@ -57,7 +65,7 @@ class OperateController extends Controller
             $rows = DB::select($sql);
             $news = array();
             
-            foreach ($rows as $k => $v) {
+            foreach($rows as $k => $v){
                 $news[$k]['id'] = $v->id;
                 $news[$k]['realname'] = $v->realname;
                 $news[$k]['company_id'] = $v->company_id;
@@ -67,51 +75,22 @@ class OperateController extends Controller
                 $news[$k]['join_time'] = $v->join_time;
                 $news[$k]['login_time'] = $v->login_time;
                 $news[$k]['is_admin'] = $v->is_admin;
-                $news[$k]['cur_month_d'] = $this->getCurMonth($v->company_id, $v->user_id)[0]; // $v->cur_month_d;
-                $news[$k]['total_d'] = $this->getTotal($v->company_id, $v->user_id, 0); // $v->total_d;
-                $news[$k]['cur_month_c'] = $this->getCurMonth($v->company_id, $v->user_id)[1]; // $v->cur_month_c;
-                $news[$k]['total_c'] = $this->getTotal($v->company_id, $v->user_id, 1); // $v->total_c;
-                $news[$k]['total_j_d'] = $this->getTotalJ($v->company_id, $v->user_id, 0); // sprintf("%.0f", $v->total_j_d);
-                $news[$k]['total_j_c'] = $this->getTotalJ($v->company_id, $v->user_id, 1); // sprintf("%.0f", $v->total_j_c);
+                $news[$k]['cur_month_d'] = $v->cur_month_d;
+                $news[$k]['total_d'] = $v->total_d;
+                $news[$k]['cur_month_c'] = $v->cur_month_c;
+                $news[$k]['total_c'] = $v->total_c;
+                $news[$k]['total_j_d'] = sprintf("%.0f", $v->total_j_d);
+                $news[$k]['total_j_c'] = sprintf("%.0f", $v->total_j_c);
             }
             $data['rows'] = $news;
+        
             return response()->json($data);
+
         }
         return view('admin.operate.user');
     }
 
-    private function getCurMonth($company_id, $user_id) {
-        $result = [0, 0];
-        $rows = AdminLog::select('id, created_at')
-                ->where('company_id', $company_id)
-                ->where('user_id', $user_id)
-                ->get();
-        if (count($rows) > 0) {
-            foreach ($rows as $k => $v) {
-                if ($type == 0) {
-                    $result[1] += 1;
-                } else if ($type == 1) {
-                    $result[1] += 1;
-                }
-            }
-        }
-        return $result;
-    }
-
-    private function getTotal($company_id, $user_id, $type) {
-        return AdminLog::select('id')
-                ->where('company_id', $company_id)
-                ->where('user_id', $user_id)
-                ->where('type', $type)
-                ->count();
-    }
-
-    private function getTotalJ($company_id, $user_id, $type) {
-        return 0;
-    }
-
-    public function userinfo(Request $request) {
-        
+    public function userinfo(Request $request){
         if ($request->ajax()) {
             $sortName = $request->post("sortName");    //排序列名
             $sortOrder = $request->post("sortOrder");   //排序（desc，asc）
@@ -149,9 +128,8 @@ class OperateController extends Controller
         return view('admin.operate.userinfo',['user_id' => $request->user_id, 'company_id' => $request->company_id]);
     }
 
-    public function company(Request $request) {
-        
-        if ($request->ajax()) {
+    public function company(Request $request){
+        if($request->ajax()){
 
             $sortName = $request->post("sortName");    //排序列名
             $sortOrder = $request->post("sortOrder");   //排序（desc，asc）
@@ -163,22 +141,38 @@ class OperateController extends Controller
                 $sortName = 'c.id';
             }
 
-            $sql = "select c.id,c.company_name,c.created_at,cu.login_time,m.realname";
-            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_d";
-            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 0 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1) as last_month_d";
-            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 0) as total_d";
-            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_c";
-            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 1 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1) as last_month_c";
-            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 1) as total_c";
-            $sql .= ",(((select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 0 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1))/2) as total_j_d";
-            $sql .= ",(((select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 1 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1))/2) as total_j_c";
-            $sql .= ",(select count(id) from company_user where company_id = c.id)  as account_number";
-            $sql .= " from company as c inner join company_user as cu on cu.company_id=c.id";
-            $sql .= " inner join members as m on m.id=cu.user_id where cu.is_admin=1";
+//            $sql = "select c.id,c.company_name,c.created_at,cu.login_time,m.realname";
+//            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_d";
+//            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 0 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1) as last_month_d";
+//            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 0) as total_d";
+//            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m')) as cur_month_c";
+//            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 1 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1) as last_month_c";
+//            $sql .= ",(select count(al.id) from admin_log as al where c.id=al.company_id and type = 1) as total_c";
+//            $sql .= ",(((select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 0 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 0 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1))/2) as total_j_d";
+//            $sql .= ",(((select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 1 and DATE_FORMAT(created_at,'%Y%m') = DATE_FORMAT(CURDATE(),'%Y%m'))+(select count(al.id) from admin_log as al where cu.user_id = al.user_id and type = 1 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(created_at,'%Y%m'))=1))/2) as total_j_c";
+//            $sql .= ",(select count(id) from company_user where company_id = c.id)  as account_number";
+//            $sql .= " from company as c inner join company_user as cu on cu.company_id=c.id";
+//            $sql .= " inner join members as m on m.id=cu.user_id where cu.is_admin=1";
 
-            if ($search) {
+
+            $sql = "select c.id,c.company_name,c.created_at,cu.login_time,m.realname,";
+            $sql .= "sum((case when al.type = 0 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end)) as cur_month_d,";
+            $sql .= "sum((case when al.type = 0 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(al.created_at,'%Y%m'))=1 then 1 else 0 end)) as last_month_d,";
+            $sql .= "sum((case when al.type = 0  then 1 else 0 end)) as total_d,";
+            $sql .= "sum((case when al.type = 1 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end)) as cur_month_c,";
+            $sql .= "sum((case when al.type = 1 and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(al.created_at,'%Y%m'))=1 then 1 else 0 end)) as last_month_c,";
+            $sql .= "sum((case when al.type = 1  then 1 else 0 end)) as total_c,";
+            $sql .= "(sum((case when al.type = 0 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end))+ sum((case when al.type = 0  and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(al.created_at,'%Y%m'))=1 then 1 else 0 end)))/2  as total_j_d,";
+            $sql .= "(sum((case when al.type = 1 and DATE_FORMAT(al.created_at, '%Y%m') = DATE_FORMAT(CURDATE(), '%Y%m') then 1 else 0 end))+ sum((case when al.type = 1  and PERIOD_DIFF(date_format(now(),'%Y%m'),date_format(al.created_at,'%Y%m'))=1 then 1 else 0 end)))/2  as total_j_c,";
+            $sql .= "(select count(id) from company_user where company_id = c.id) as account_number ";
+            $sql .= " from company as c inner join company_user as cu on cu.company_id=c.id inner join members as m on cu.user_id=m.id left join admin_log as al on (cu.company_id = al.company_id and cu.user_id = al.user_id)";
+            $sql .= " where cu.is_admin=1";
+
+            if($search){
                 $sql .= " and (c.company_name like '%". $search ."%' or m.realname like '%". $search ."%')";
             }
+
+            $sql .= " group by c.id,c.company_name,c.created_at,cu.login_time,m.realname";
 
             $data['total'] = count(DB::select($sql));
 
@@ -191,7 +185,7 @@ class OperateController extends Controller
             $rows = DB::select($sql);
             $news = array();
             
-            foreach ($rows as $k => $v) {
+            foreach($rows as $k => $v){
                 $news[$k]['id'] = $v->id;
                 $news[$k]['company_name'] = $v->company_name;
                 $news[$k]['created_at'] = $v->created_at;
@@ -210,12 +204,12 @@ class OperateController extends Controller
             $data['rows'] = $news;
           
             return response()->json($data);
+
         }
         return view('admin.operate.company');
     }
 
-    public function companyinfo(Request $request) {
-        
+    public function companyinfo(Request $request){
         if ($request->ajax()) {
             $sortName = $request->post("sortName");    //排序列名
             $sortOrder = $request->post("sortOrder");   //排序（desc，asc）
@@ -246,8 +240,8 @@ class OperateController extends Controller
             $data['rows'] = $query->orderBy($sortName, $sortOrder)->get();
 
             return response()->json($data);
+
         }
         return view('admin.operate.companyinfo',['company_id' => $request->company_id]);
     }
-    
 }
